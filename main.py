b1023e07017ba9bb4553235fab87c6e72a27d3aa
dotdevdotdev <dotdevdotdev@gmail.com>
2024-10-04 07:02:19 -0400

    cors cors cors

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import os
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from bson import ObjectId
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Initialize MongoDB client
 MONGODB_URI = os.environ.get("MONGODB_URI")
